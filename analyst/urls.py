5c8c69ccb7f7ff00aa082638ec53d6d8c57584f1
Rishi <sankarrishi741@gmail.com>
2026-09-26 15:13:26 +0530

    Resolve merge conflict

--- analyst/urls.py
+++ analyst/urls.py
@@ -11,14 +11,9 @@ urlpatterns = [
     path('records/',views.records),
     path('analys/',views.analys),
     path('report/',views.report),
-<<<<<<< HEAD
     path('verify/',views.verify),
-<<<<<<< Updated upstream
-=======
->>>>>>> a3ec0e37fed7a60b9c57d1f1f5cc2f52bca2d287
-    path('threat/',views.threat)
-=======
     path('threat/',views.threat),
->>>>>>> Stashed changes
+    path('threat/',views.threat),
+
 ]
 
